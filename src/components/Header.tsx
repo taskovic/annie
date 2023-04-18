d5302b92a37efae0c381d7a6655725cd27082643
taskovic <taskovic.development@gmail.com>
2023-04-18 19:23:21 +0200

    added logic for switching navigation tabs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,6 @@
-import { useContext } from "react"
-import { Context } from "../contexts"
-import { TContext } from "../types";
-import { navbarTabs } from "../configs";
-
-
 export default function Header() {
-  const { 
-    activeTabName, 
-    setActiveTabName 
-  } = useContext(Context) as TContext;
 
   return (
-    <div className="annie-header" onClick={() =>setActiveTabName("sadas") }></div>
+    <div className="annie-header"></div>
   )
 }
--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -3,44 +3,54 @@
  * Inside the screen will be complete business logic alongs with screen state management
  * Screen style will be just grid styling, the basic screen style will be done in components
  */
-import Header from "../components/Header"
-import {  
+import Header from "../components/Header";
+import React, {  
   useState, 
   useEffect 
-} from "react"
+} from "react";
 import { Context } from "../contexts";
-import Filters from "../components/Filters";
-
+import { navbarTabs } from "../configs";
 
 export default function Dashboard() {
-  const [activeTabName, setActiveTabName] = useState("first_tab");
+  const [activeTabName, setActiveTabName] = useState(navbarTabs[0]);
 
   useEffect(() => {
     // Do something on component init
 
     return () => {
-      // reset all states or screen store
+      setActiveTabName(navbarTabs[0]);
     }
-  }, [])
+  }, []);
 
-  function handleTabClick() {}
+  function getComponent(name: string) {
+    if (!name) return <h1>No View</h1> // this can be loader
+
+    switch(name) {
+      case navbarTabs[0]:
+        return () => <h1>View 1</h1> // Change this with proper component which will be called
+      case navbarTabs[1]:
+        return () => <h1>View 2</h1> // Change this with proper component which will be called
+      case navbarTabs[2]:
+        return () => <h1>View 3</h1> // Change this with proper component which will be called
+      case navbarTabs[3]:
+        return () => <h1>View 4</h1> // Change this with proper component which will be called
+      default: 
+        return () => <h1>No View</h1> // Change this with proper component which will be called
+    }
+  };
+
+  const Component: React.FunctionComponent = getComponent(activeTabName);
 
   const ProviderRegistry = {
-    handleTabClick,
     activeTabName,
     setActiveTabName
   }
-
-
 
   return(
     <Context.Provider value={ProviderRegistry}>
       <div className="annie-dashboard">
         <Header />
-        <Filters>
-          
-        </Filters>
-        
+        <Component />
       </div>  
     </Context.Provider>
   )
