fb7b88f1b689ded39053bd3e687deeb91a4c2b5d
MarkoS1998 <marko.stam1998@gmail.com>
2023-04-20 16:42:44 +0200

    navigation bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
 import Logo from "./Logo";
-import {Grid, TextField, styled } from "@mui/material"
+import { Box, Tab } from "@mui/material"
 import SearchInput from "./SearchInput";
-
+import NotificationButton from "./NotificationButton";
+import ProfileButton from "./ProfileButton";
+import { navbarTabs } from "../configs";
+import NavBar from "./NavBar";
 
 export default function Header() {
 
+  const [value, setValue] = useState(navbarTabs[0]);
+  const [notification, setNotification] = useState([{tab: navbarTabs[3],value: 3}]);
+
+  const handleChangeTab = (value: string) =>{
+    setValue(value)
+    setNotification(notification.filter(x => x.tab !== value))
+  }
+
   return (
-    <Grid className="annie-header">
+    <Box className="annie-header">
       <Logo />
-      <Grid className="search-section">
-        <SearchInput placeholder="Search"/>
-      </Grid>
-      
-    </Grid>
+      <Box className="search-section">
+        <SearchInput placeholder="Search" />
+      </Box>
+      <Box className="notification-section">
+        <NotificationButton onClick={() => { alert("Notification") }} />
+      </Box>
+      <Box className="profile-section">
+        <ProfileButton onClick={() => { alert("Profile") }} />
+      </Box>
+      <Box className="navbar-section">
+        <NavBar
+          value={value}
+          tabs={navbarTabs}
+          notifications={notification}
+          handleChange={setValue}
+        />
+      </Box>
+    </Box>
   )
 }
